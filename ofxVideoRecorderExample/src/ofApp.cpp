0772c709dd88caeb8c5adb61e406b46bc47e8ed2
Brian Eschrich <hi@brinoausrino.com>
2020-09-10 13:00:37 +0200

    updated for windows

--- ofxVideoRecorderExample/src/ofApp.cpp
+++ ofxVideoRecorderExample/src/ofApp.cpp
@@ -9,19 +9,18 @@ void ofApp::setup(){
     ofSetLogLevel(OF_LOG_VERBOSE);
     vidGrabber.setDesiredFrameRate(30);
     vidGrabber.initGrabber(640, 480);
-//    vidRecorder.setFfmpegLocation(ofFilePath::getAbsolutePath("ffmpeg")); // use this is you have ffmpeg installed in your data folder
-
+#ifdef TARGET_WIN32
+	vidRecorder.setFfmpegLocation("C:\\ffmpeg\\bin\\"); // use this is you have ffmpeg installed in your data folder
+#endif
     fileName = "testMovie";
-    fileExt = ".mov"; // ffmpeg uses the extension to determine the container type. run 'ffmpeg -formats' to see supported formats
+    fileExt = ".mp4"; // ffmpeg uses the extension to determine the container type. run 'ffmpeg -formats' to see supported formats
 
     // override the default codecs if you like
     // run 'ffmpeg -codecs' to find out what your implementation supports (or -formats on some older versions)
-    vidRecorder.setVideoCodec("mpeg4");
+    vidRecorder.setVideoCodec("mpeg4"); 
     vidRecorder.setVideoBitrate("800k");
     vidRecorder.setAudioCodec("mp3");
     vidRecorder.setAudioBitrate("192k");
-
-    ofAddListener(vidRecorder.outputFileCompleteEvent, this, &ofApp::recordingComplete);
 
 //    soundStream.listDevices();
 //    soundStream.setDeviceID(11);
@@ -32,9 +31,7 @@ void ofApp::setup(){
     ofEnableAlphaBlending();
 }
 
-//--------------------------------------------------------------
-void ofApp::exit(){
-    ofRemoveListener(vidRecorder.outputFileCompleteEvent, this, &ofApp::recordingComplete);
+void ofApp::exit() {
     vidRecorder.close();
 }
 
@@ -42,17 +39,17 @@ void ofApp::exit(){
 void ofApp::update(){
     vidGrabber.update();
     if(vidGrabber.isFrameNew() && bRecording){
-        bool success = vidRecorder.addFrame(vidGrabber.getPixels());
+        bool success = vidRecorder.addFrame(vidGrabber.getPixelsRef());
         if (!success) {
             ofLogWarning("This frame was not added!");
         }
     }
-
+    
     // Check if the video recorder encountered any error while writing video frame or audio smaples.
     if (vidRecorder.hasVideoError()) {
         ofLogWarning("The video recorder failed to write some frames!");
     }
-
+    
     if (vidRecorder.hasAudioError()) {
         ofLogWarning("The video recorder failed to write some audio samples!");
     }
@@ -71,25 +68,19 @@ void ofApp::draw(){
     << (bRecording?"close current video file: c":"") << endl;
 
     ofSetColor(0,0,0,100);
-    ofDrawRectangle(0, 0, 260, 75);
+    ofRect(0, 0, 260, 75);
     ofSetColor(255, 255, 255);
     ofDrawBitmapString(ss.str(),15,15);
 
     if(bRecording){
     ofSetColor(255, 0, 0);
-    ofDrawCircle(ofGetWidth() - 20, 20, 5);
+    ofCircle(ofGetWidth() - 20, 20, 5);
     }
 }
 
-//--------------------------------------------------------------
 void ofApp::audioIn(float *input, int bufferSize, int nChannels){
     if(bRecording)
         vidRecorder.addAudioSamples(input, bufferSize, nChannels);
-}
-
-//--------------------------------------------------------------
-void ofApp::recordingComplete(ofxVideoRecorderOutputFileCompleteEventArgs& args){
-    cout << "The recoded video file is now complete." << endl;
 }
 
 //--------------------------------------------------------------
@@ -106,7 +97,7 @@ void ofApp::keyReleased(int key){
 //          vidRecorder.setup(fileName+ofGetTimestampString()+fileExt, vidGrabber.getWidth(), vidGrabber.getHeight(), 30); // no audio
 //            vidRecorder.setup(fileName+ofGetTimestampString()+fileExt, 0,0,0, sampleRate, channels); // no video
 //          vidRecorder.setupCustomOutput(vidGrabber.getWidth(), vidGrabber.getHeight(), 30, sampleRate, channels, "-vcodec mpeg4 -b 1600k -acodec mp2 -ab 128k -f mpegts udp://localhost:1234"); // for custom ffmpeg output string (streaming, etc)
-
+            
             // Start recording
             vidRecorder.start();
         }
--- src/ofxVideoRecorder.h
+++ src/ofxVideoRecorder.h
@@ -4,206 +4,238 @@
 #include "Poco/Condition.h"
 #include <set>
 
-//--------------------------------------------------------------
-//--------------------------------------------------------------
+#ifdef TARGET_WIN32
+#include <windows.h> 
+#include <stdio.h>
+#include <conio.h>
+#include <tchar.h>
+#endif
+
 template <typename T>
 struct lockFreeQueue {
-    lockFreeQueue(){
-        list.push_back(T());
-        iHead = list.begin();
-        iTail = list.end();
-    }
-    void Produce(const T& t){
-        list.push_back(t);
-        iTail = list.end();
-        list.erase(list.begin(), iHead);
-    }
-    bool Consume(T& t){
-        typename TList::iterator iNext = iHead;
-        ++iNext;
-        if (iNext != iTail)
-        {
-            iHead = iNext;
-            t = *iHead;
-            return true;
-        }
-        return false;
-    }
-    int size() { return distance(iHead,iTail)-1; }
-    typename std::list<T>::iterator getHead() { return iHead; }
-    typename std::list<T>::iterator getTail() { return iTail; }
+	lockFreeQueue() {
+		list.push_back(T());
+		iHead = list.begin();
+		iTail = list.end();
+	}
+	void Produce(const T& t) {
+		list.push_back(t);
+		iTail = list.end();
+		list.erase(list.begin(), iHead);
+	}
+	bool Consume(T& t) {
+		typename TList::iterator iNext = iHead;
+		++iNext;
+		if (iNext != iTail)
+		{
+			iHead = iNext;
+			t = *iHead;
+			return true;
+		}
+		return false;
+	}
+	int size() { return distance(iHead, iTail) - 1; }
+	typename std::list<T>::iterator getHead() { return iHead; }
+	typename std::list<T>::iterator getTail() { return iTail; }
+	
+	void clear() {
+		list.clear();
+	}
 
 
 private:
-    typedef std::list<T> TList;
-    TList list;
-    typename TList::iterator iHead, iTail;
+	typedef std::list<T> TList;
+	TList list;
+	typename TList::iterator iHead, iTail;
 };
 
-class execThread : public ofThread{
+class execThread : public ofThread {
 public:
-    execThread();
-    void setup(string command);
-    void threadedFunction();
-    bool isInitialized() { return initialized; }
+	execThread();
+	void setup(string command);
+	void threadedFunction();
 private:
-    string execCommand;
-    bool initialized;
+	string execCommand;
 };
 
 struct audioFrameShort {
-    short * data;
-    int size;
+	short * data;
+	int size;
 };
 
-//--------------------------------------------------------------
-//--------------------------------------------------------------
 class ofxVideoDataWriterThread : public ofThread {
 public:
-    ofxVideoDataWriterThread();
-//    void setup(ofFile *file, lockFreeQueue<ofPixels *> * q);
-    void setup(string filePath, lockFreeQueue<ofPixels *> * q);
-    void threadedFunction();
-    void signal();
-    void setPipeNonBlocking();
-    bool isWriting() { return bIsWriting; }
-    void close() { bClose = true; stopThread(); signal(); }
-    bool bNotifyError;
+	ofxVideoDataWriterThread();
+#if defined( TARGET_OSX ) || defined( TARGET_LINUX )
+	void setup(string filePath, lockFreeQueue<ofPixels *> * q);
+#endif
+#ifdef TARGET_WIN32
+	void setup(HANDLE pipeHandle, lockFreeQueue<shared_ptr< ofPixels>> * q);
+	//void setup(HANDLE pipeHandle, lockFreeQueue<ofPixels *> * q);
+	HANDLE videoHandle;
+	HANDLE fileHandle;
+#endif
+	void threadedFunction();
+	void signal();
+	void setPipeNonBlocking();
+	bool isWriting() { return bIsWriting; }
+	void close() { bClose = true; stopThread(); signal(); }
 private:
-    ofMutex conditionMutex;
-    Poco::Condition condition;
-//    ofFile * writer;
-    string filePath;
-    int fd;
-    lockFreeQueue<ofPixels *> * queue;
-    bool bIsWriting;
-    bool bClose;
+	ofMutex conditionMutex;
+	Poco::Condition condition;
+	string filePath;
+	int fd;
+	lockFreeQueue<shared_ptr<ofPixels>> * queue;
+	//lockFreeQueue<ofPixels *> * queue;
+	bool bIsWriting;
+	bool bClose;
 };
 
-//--------------------------------------------------------------
-//--------------------------------------------------------------
 class ofxAudioDataWriterThread : public ofThread {
 public:
-    ofxAudioDataWriterThread();
-//    void setup(ofFile *file, lockFreeQueue<audioFrameShort *> * q);
-    void setup(string filePath, lockFreeQueue<audioFrameShort *> * q);
-    void threadedFunction();
-    void signal();
-    void setPipeNonBlocking();
-    bool isWriting() { return bIsWriting; }
-    void close() { bClose = true; stopThread(); signal();  }
-    bool bNotifyError;
+	ofxAudioDataWriterThread();
+#if defined( TARGET_OSX ) || defined( TARGET_LINUX )
+	void setup(string filePath, lockFreeQueue<audioFrameShort *> * q);
+#endif
+#ifdef TARGET_WIN32
+	void setup(HANDLE pipeHandle, lockFreeQueue<audioFrameShort *> * q);
+	HANDLE audioHandle;
+	HANDLE fileHandle;
+#endif
+	void threadedFunction();
+	void signal();
+	void setPipeNonBlocking();
+	bool isWriting() { return bIsWriting; }
+	void close() { bClose = true; stopThread(); signal(); }
 private:
-    ofMutex conditionMutex;
-    Poco::Condition condition;
-//    ofFile * writer;
-    string filePath;
-    int fd;
-    lockFreeQueue<audioFrameShort *> * queue;
-    bool bIsWriting;
-    bool bClose;
+	ofMutex conditionMutex;
+	Poco::Condition condition;
+	string filePath;
+	int fd;
+	lockFreeQueue<audioFrameShort *> * queue;
+	bool bIsWriting;
+	bool bClose;
 };
 
-
-//--------------------------------------------------------------
-//--------------------------------------------------------------
-class ofxVideoRecorderOutputFileCompleteEventArgs
-: public ofEventArgs
+class ofxVideoRecorder
 {
 public:
-    string fileName;
-};
+	ofxVideoRecorder();
+	bool setup(string fname, int w, int h, float fps, int sampleRate = 0, int channels = 0, bool sysClockSync = false, bool silent = false);
+	bool setupCustomOutput(int w, int h, float fps, string outputString, bool sysClockSync = false, bool silent = false);
+	bool setupCustomOutput(int w, int h, float fps, int sampleRate, int channels, string outputString,bool sysClockSync = false, bool silent = false);
+	bool runCustomScript(string script);
+	bool addFrame(const ofPixels &pixels);
+	void addAudioSamples(float * samples, int bufferSize, int numChannels);
 
-//--------------------------------------------------------------
-//--------------------------------------------------------------
-class ofxVideoRecorder  : public ofThread
-{
-public:
-    ofxVideoRecorder();
+	void start();
+	void close();
+	void setPaused(bool bPause);
 
-    void threadedFunction();
+	void setFfmpegLocation(string loc) {
+		ofDirectory dir(loc);
+		ffmpegLocation = dir.getAbsolutePath() + "\\ffmpeg.exe"; 
+	}
+	void setMovFileExtension(string extension) { movFileExt = extension; }
+	void setAudioFileExtension(string extension) { audioFileExt = extension; }
+	void setVideoCodec(string codec) { videoCodec = codec; }
+	void setVideoBitrate(string bitrate) { videoBitrate = bitrate; }
 
-    ofEvent<ofxVideoRecorderOutputFileCompleteEventArgs> outputFileCompleteEvent;
+	void setPixelFormat(string pixelF) { //rgb24 || gray, default is rgb24
+		pixelFormat = pixelF;
+	};
 
-    bool setup(string fname, int w, int h, float fps, int sampleRate=0, int channels=0, bool sysClockSync=false, bool silent=false);
-    bool setupCustomOutput(int w, int h, float fps, string outputString, bool sysClockSync=false, bool silent=false);
-    bool setupCustomOutput(int w, int h, float fps, int sampleRate, int channels, string outputString, bool sysClockSync=false, bool silent=false);
+	unsigned long long getNumVideoFramesRecorded() { return videoFramesRecorded; }
+	unsigned long long getNumAudioSamplesRecorded() { return audioSamplesRecorded; }
 
-    bool addFrame(const ofPixels &pixels);
-    void addAudioSamples(float * samples, int bufferSize, int numChannels);
+	int getVideoQueueSize() { return frames.size(); }
+	int getAudioQueueSize() { return audioFrames.size(); }
 
-    void start();
-    void close();
-    void setPaused(bool bPause);
+	bool isInitialized() { return bIsInitialized; }
+	bool isRecording() { return bIsRecording; };
+	bool isPaused() { return bIsPaused; };
+	bool isSyncAgainstSysClock() { return bSysClockSync; };
 
-    bool hasVideoError();
-    bool hasAudioError();
+	string getMoviePath() { return filePath; }
+	int getWidth() { return width; }
+	int getHeight() { return height; }
 
-    void setFfmpegLocation(string loc) { ffmpegLocation = loc; }
-    void setVideoCodec(string codec) { videoCodec = codec; }
-    void setAudioCodec(string codec) { audioCodec = codec; }
-    void setVideoBitrate(string bitrate) { videoBitrate = bitrate; }
-    void setAudioBitrate(string bitrate) { audioBitrate = bitrate; }
-
-    void setPixelFormat( string pixelF){ //rgb24 || gray, default is rgb24
-        pixelFormat = pixelF;
-    };
-    void setOutputPixelFormat(string pixelF) {
-        outputPixelFormat = pixelF;
-    }
-
-    unsigned long long getNumVideoFramesRecorded() { return videoFramesRecorded; }
-    unsigned long long getNumAudioSamplesRecorded() { return audioSamplesRecorded; }
-
-    int getVideoQueueSize(){ return frames.size(); }
-    int getAudioQueueSize(){ return audioFrames.size(); }
-
-    bool isInitialized(){ return bIsInitialized; }
-    bool isRecording() { return bIsRecording; };
-    bool isPaused() { return bIsPaused; };
-    bool isSyncAgainstSysClock() { return bSysClockSync; };
-
-    string getMoviePath(){ return moviePath; }
-    int getWidth(){return width;}
-    int getHeight(){return height;}
+	void clearFrames();
 
 private:
-    string fileName;
-    string moviePath;
-    string videoPipePath, audioPipePath;
-    string ffmpegLocation;
-    string videoCodec, audioCodec, videoBitrate, audioBitrate, pixelFormat, outputPixelFormat;
-    int width, height, sampleRate, audioChannels;
-    float frameRate;
 
-    bool bIsInitialized;
-    bool bRecordAudio;
-    bool bRecordVideo;
-    bool bIsRecording;
-    bool bIsPaused;
-    bool bFinishing;
-    bool bIsSilent;
+	std::wstring convertNarrowToWide(const std::string& as) {
+		if (as.empty())    return std::wstring();
+		size_t reqLength = ::MultiByteToWideChar(CP_UTF8, 0, as.c_str(), (int)as.length(), 0, 0);
+		std::wstring ret(reqLength, L'\0');
+		::MultiByteToWideChar(CP_UTF8, 0, as.c_str(), (int)as.length(), &ret[0], (int)ret.length());
+		return ret;
+	}
 
-    bool bSysClockSync;
-    float startTime;
-    float recordingDuration;
-    float totalRecordingDuration;
-    float systemClock();
+	wchar_t *convertCharArrayToLPCWSTR(const char* charArray)
+	{
+		wchar_t* wString = new wchar_t[4096];
+		MultiByteToWideChar(CP_ACP, 0, charArray, -1, wString, 4096);
+		return wString;
+	}
 
-    lockFreeQueue<ofPixels *> frames;
-    lockFreeQueue<audioFrameShort *> audioFrames;
-    unsigned long long audioSamplesRecorded;
-    unsigned long long videoFramesRecorded;
-    ofxVideoDataWriterThread videoThread;
-    ofxAudioDataWriterThread audioThread;
-    execThread ffmpegThread;
-//    ofFile videoPipe, audioPipe;
-    int videoPipeFd, audioPipeFd;
-    int pipeNumber;
+	std::string convertWideToNarrow(const wchar_t *s, char dfault = '?',
+		const std::locale& loc = std::locale())
+	{
+		std::ostringstream stm;
 
-    static set<int> openPipes;
-    static int requestPipeNumber();
-    static void retirePipeNumber(int num);
+		while (*s != L'\0') {
+			stm << std::use_facet< std::ctype<wchar_t> >(loc).narrow(*s++, dfault);
+		}
+		return stm.str();
+	}
 
-    void outputFileComplete();
+	string filePath;
+	string fileName;
+	string movFileExt;
+	string audioFileExt;
+	string videoPipePath, audioPipePath;
+	string ffmpegLocation;
+	string videoCodec, audioCodec, videoBitrate, audioBitrate, pixelFormat;
+	int width, height, sampleRate, audioChannels;
+	float frameRate;
+
+	bool bIsInitialized;
+	bool bRecordAudio;
+	bool bRecordVideo;
+	bool bIsRecording;
+	bool bIsPaused;
+	bool bFinishing;
+	bool bIsSilent;
+
+	bool bSysClockSync;
+	float startTime;
+	float recordingDuration;
+	float totalRecordingDuration;
+	float systemClock();
+
+	//lockFreeQueue<ofPixels *> frames;
+	lockFreeQueue<shared_ptr<ofPixels>> frames;
+	lockFreeQueue<audioFrameShort *> audioFrames;
+	unsigned long long audioSamplesRecorded;
+	unsigned long long videoFramesRecorded;
+	ofxVideoDataWriterThread videoThread;
+	ofxAudioDataWriterThread audioThread;
+	execThread ffmpegThread;
+	execThread ffmpegVideoThread;
+	execThread ffmpegAudioThread;
+	bool vThreadRunning, aThreadRunning;
+	int videoPipeFd, audioPipeFd;
+	int pipeNumber;
+
+	static set<int> openPipes;
+	static int requestPipeNumber();
+	static void retirePipeNumber(int num);
+
+#ifdef TARGET_WIN32
+	HANDLE hVPipe;
+	HANDLE hAPipe;
+	LPTSTR vPipename;
+	LPTSTR aPipename;
+#endif
 };
